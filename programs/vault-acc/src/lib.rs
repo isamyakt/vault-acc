77c87a2b1e9e9a3602ac32216082f899608cd937
isamyakt <isamyakt17@gmail.com>
2024-02-26 23:12:19 +0530

    SystemAccount deposit and withdrawl for vault acc

--- programs/vault-acc/src/lib.rs
+++ programs/vault-acc/src/lib.rs
@@ -1,9 +1,11 @@
 use anchor_lang::prelude::*;
+use anchor_lang::system_program::{Transfer, transfer};
 
 declare_id!("9pU1ESxdgeWPKjQwg6R7vuVom8rno7L8CNb76EUh6vVN");
 
 #[program]
 pub mod vault_acc {
+
     use super::*;
 
     pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
@@ -11,6 +13,43 @@ pub mod vault_acc {
         ctx.accounts.state.vault_bump = ctx.bumps.vault;
         ctx.accounts.state.state_bump = ctx.bumps.state;
         Ok(())
+    }
+
+    pub fn deposit(ctx: Context<Payment>, amount: u64) -> Result<()> {
+        let accounts = Transfer {
+            from: ctx.accounts.owner.to_account_info(),
+            to: ctx.accounts.vault.to_account_info()
+        };
+
+        let cpi = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(), 
+            accounts
+        );
+
+        transfer(cpi, amount)
+    }
+
+    pub fn withdraw(ctx: Context<Payment>, amount: u64) -> Result<()> {
+        let accounts = Transfer {
+            from: ctx.accounts.vault.to_account_info(),
+            to: ctx.accounts.owner.to_account_info(),
+        };
+
+        let seeds = &[
+            b"auth",
+            ctx.accounts.state.to_account_info().key.as_ref(),
+            &[ctx.accounts.state.auth_bump]
+        ];
+
+        let pda_signer = &[&seeds[..]];
+
+        let cpi = CpiContext::new_with_signer(
+            ctx.accounts.system_program.to_account_info(), 
+            accounts, 
+            pda_signer
+        );
+
+        transfer(cpi, amount)
     }
 }
 
@@ -38,6 +77,34 @@ pub struct Initialize<'info> {
     #[account(
         seeds=[b"vault", state.key().as_ref()],
         bump
+    )]
+    vault: SystemAccount<'info>,
+
+    system_program: Program<'info, System>
+}
+
+#[derive(Accounts)]
+pub struct Payment<'info> {
+    #[account(mut)]
+    owner: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds=[b"state", owner.key().as_ref()],
+        bump=state.state_bump,
+    )]
+    state: Account<'info, VaultState>,
+
+    #[account(
+        seeds=[b"auth", state.key().as_ref()],
+        bump=state.auth_bump,
+    )]
+    /// CHECK: This is safe
+    auth: UncheckedAccount<'info>,
+
+    #[account(
+        seeds=[b"vault", state.key().as_ref()],
+        bump=state.vault_bump
     )]
     vault: SystemAccount<'info>,
 
